dc9e768ef7a54d893f6790e149c4398cff328e9a
Hamza Khan <125295517+HamzaK702@users.noreply.github.com>
2024-07-11 16:06:52 +0500

    orders/product apis edited

--- backend/services/ShopifyService.js
+++ backend/services/ShopifyService.js
@@ -40,58 +40,211 @@ export class ShopifyService {
             body: JSON.stringify({ query })
         });
         const data = await response.json();
+        console.log(data)
         return data;
     }
 
     static async getOrders(shop, token) {
         const query = `
-            {
-                orders(first: 10) {
+          {
+            orders(first: 10) {
+              edges {
+                node {
+                  id
+                  billingAddress {
+                    id
+                    address1
+                    address2
+                    city
+                    company
+                    country
+                    countryCode
+                    countryCodeV2
+                    firstName
+                    lastName
+                    latitude
+                    longitude
+                    name
+                    phone
+                    province
+                    provinceCode
+                    zip
+                  }
+                  cancelReason
+                  cancelledAt
+                  closed
+                  closedAt
+                  createdAt
+                  currencyCode
+                  currentSubtotalLineItemsQuantity
+                  customAttributes {
+                    key
+                    value
+                  }
+                  customer {
+                    id
+                    acceptsMarketing
+                    email
+                    firstName
+                    lastName
+                    phone
+                    tags
+                  }
+                  customerLocale
+                  displayFinancialStatus
+                  displayFulfillmentStatus
+                  email
+                  fulfillmentOrders(first: 10) {
                     edges {
-                        node {
-                            id
-                            name
-                            totalPrice
-                            createdAt
-                            lineItems(first: 5) {
-                                edges {
-                                    node {
-                                        name
-                                        quantity
-                                        price
-                                    }
-                                }
-                            }
-                        }
+                      node {
+                        id
+                        status
+                      }
                     }
+                  }
+                  lineItems(first: 250) {
+                    edges {
+                      node {
+                        id
+                        title
+                        quantity
+                        product {
+                          id
+                          title
+                          handle
+                        }
+                      }
+                    }
+                  }
+                  name
+                  note
+                  phone
+                  processedAt
+                  shippingAddress {
+                    id
+                    address1
+                    address2
+                    city
+                    company
+                    country
+                    countryCode
+                    countryCodeV2
+                    firstName
+                    lastName
+                    latitude
+                    longitude
+                    name
+                    phone
+                    province
+                    provinceCode
+                    zip
+                  }
+                  subtotalPrice
+                  tags
+                  taxLines {
+                    price
+                    rate
+                    title
+                  }
+                  totalPrice
+                  totalRefunded
+                  updatedAt
                 }
+              }
             }
+          }
         `;
-        return await this.fetchGraphQL(shop, token, query);
+        const orders = await this.fetchGraphQL(shop, token, query);
+        return orders;
     }
+    
+    
 
-    static async getProducts(shop, token) {
+      static async getProducts(shop, token) {
         const query = `
-            {
-                products(first: 10) {
+          {
+            products(first: 10) {
+              edges {
+                node {
+                  id
+                  adminGraphqlApiId
+                  availableForSale
+                  createdAt
+                  description
+                  descriptionHtml
+                  onlineStoreUrl
+                  options {
+                    id
+                    name
+                    values
+                  }
+                  productType
+                  publishedAt
+                  seo {
+                    description
+                    title
+                  }
+                  status
+                  tags
+                  title
+                  totalInventory
+                  updatedAt
+                  variants(first: 250) { # Increased limit to potentially get all variants
                     edges {
-                        node {
-                            id
-                            title
-                            variants(first: 5) {
-                                edges {
-                                    node {
-                                        id
-                                        title
-                                        price
-                                    }
-                                }
-                            }
+                      node {
+                        compareAtPrice
+                        displayName
+                        image {
+                          id
+                          originalSrc
+                          transformedSrc
                         }
+                        inventoryItem {
+                          id
+                          inventoryLevels(first: 250) { # Increased limit to potentially get all levels
+                            edges {
+                              node {
+                                available
+                                location {
+                                  id
+                                  name
+                                }
+                              }
+                            }
+                          }
+                          inventoryQuantity
+                        }
+                        legacyResourceId
+                        price
+                        product {
+                          id
+                        }
+                        quantityAvailable
+                        requiresShipping
+                        selectedOptions {
+                          name
+                          value
+                        }
+                        sku
+                        taxCode
+                        taxable
+                        title
+                        updatedAt
+                        weight
+                        weightUnit
+                        vendor {
+                          # Add specific vendor fields you need here
+                        }
+                      }
                     }
+                  }
                 }
+              }
             }
+          }
         `;
-        return await this.fetchGraphQL(shop, token, query);
-    }
-}
+        const products =  await this.fetchGraphQL(shop, token, query)
+        console.log(products);
+        return products;
+      }
+    }      
