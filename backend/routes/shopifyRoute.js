ba9bf0feb3a35eb31790ed4953381ebce1c53df4
Arhamalii <arhampersonal786@gmail.com>
2024-11-27 20:07:46 +0500

    shopifyRoute/install added

--- backend/routes/shopifyRoute.js
+++ backend/routes/shopifyRoute.js
@@ -1,31 +1,43 @@
-import express from 'express';
-import { ShopifyController } from '../controllers/ShopifyController.js';
+import express from "express";
+import { ShopifyController } from "../controllers/ShopifyController.js";
 
 const router = express.Router();
 //http://localhost:3001/api/shopify?shop=dumbclient.myshopify.com&userId=66bb684d7c41ca028cbeb0ab
-router.get('/shopify', (req, res) => {
-    const shop = req.query.shop;
-    const userId = req.query.userId;
-    if (shop) {
-        const state = `${userId}-${ShopifyController.nonce()}`;
-        const installUrl = ShopifyController.generateInstallUrl(shop, state);
-        console.log(state)
-        console.log(installUrl)
-        res.cookie('state', state);
-        res.redirect(installUrl);
-    } else {
-        res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
-    }
+router.get("/shopify", (req, res) => {
+  const shop = req.query.shop;
+  const userId = req.query.userId;
+  if (shop) {
+    const state = `${userId}-${ShopifyController.nonce()}`;
+    const installUrl = ShopifyController.generateInstallUrl(shop, state);
+    console.log(state);
+    console.log(installUrl);
+    res.cookie("state", state);
+    res.redirect(installUrl);
+  } else {
+    res
+      .status(400)
+      .send(
+        "Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request"
+      );
+  }
 });
 
-router.get('/shopify/callback', ShopifyController.handleAuth); //we got the token for dumb client store: shpua_9dd90273c982021d4c9bed11b7bc6e6c 
+router.post("/install", (req, res) => {
+  const { hmac, host, shop, timestamp } = req.body;
+  res
+    .status(200)
+    .send(
+      `Queries Recived hmac=${hmac}, host=${host},shop=${shop} and timestamp=${timestamp}`
+    );
+});
+
+router.get("/shopify/callback", ShopifyController.handleAuth); //we got the token for dumb client store: shpua_9dd90273c982021d4c9bed11b7bc6e6c
 // new updated token with customer fields --> shpua_9dd90273c982021d4c9bed11b7bc6e6c
-router.get('/shopdata', ShopifyController.getShopData);
+router.get("/shopdata", ShopifyController.getShopData);
 
-router.get('/shopify/orders', ShopifyController.getOrders);
-router.get('/shopify/products', ShopifyController.getProducts);
+router.get("/shopify/orders", ShopifyController.getOrders);
+router.get("/shopify/products", ShopifyController.getProducts);
 
-router.post('/bulk-operation', ShopifyController.createBulkOperation);
-
+router.post("/bulk-operation", ShopifyController.createBulkOperation);
 
 export default router;
